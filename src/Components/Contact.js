5d4b560e997f14b40f9eb526c476202c462fae6f
Fernando-Paz <fernandopaz.ipn@gmail.com>
2021-08-23 08:23:03 -0500

    worksfinals commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,7 +8,9 @@ class Contact extends Component {
     const name = this.props.data.name;
     const namesecond = this.props.data.namesecond;
     const phone = this.props.data.phone;
+    const email = this.props.data.email;
     const message = this.props.data.contactmessage;
+    const message1 = this.props.data.contactmessage1;
 
     return (
       <section id="contact">
@@ -22,97 +24,23 @@ class Contact extends Component {
 
             <div className="ten columns">
               <p className="lead">{message}</p>
-            </div>
-          </div>
-        </Fade>
-
-        <div className="row">
-          <Slide left duration={1000}>
-            <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
-                <fieldset>
-                  <div>
-                    <label htmlFor="contactName">
-                      Name <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactName"
-                      name="contactName"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
-                    <label htmlFor="contactEmail">
-                      Email <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactEmail"
-                      name="contactEmail"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
-                    <label htmlFor="contactSubject">Subject</label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactSubject"
-                      name="contactSubject"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
-                    <label htmlFor="contactMessage">
-                      Message <span className="required">*</span>
-                    </label>
-                    <textarea
-                      cols="50"
-                      rows="10"
-                      id="contactMessage"
-                      name="contactMessage"
-                    ></textarea>
-                  </div>
-
-                  <div>
-                    <button className="submit">Submit</button>
-                    <span id="image-loader">
-                      <img alt="" src="images/loader.gif" />
-                    </span>
-                  </div>
-                </fieldset>
-              </form>
-
-              <div id="message-warning"> Error boy</div>
-              <div id="message-success">
-                <i className="fa fa-check"></i>Your message was sent, thank you!
-                <br />
-              </div>
-            </div>
-          </Slide>
-
-          <Slide right duration={1000}>
-            <aside className="four columns footer-widgets">
+              <p className="lead">{message1}</p>
               <div className="widget widget_contact">
                 <h4>Phone</h4>
                 <p className="address">
                   <span>{name} {namesecond}</span>
                   <br />
                   <span>{phone}</span>
+                  <br />
+                  <span>{email}</span>
                 </p>
               </div>
-            </aside>
-          </Slide>
-        </div>
+            </div>
+            
+          </div>
+        </Fade>
+
+        
       </section>
     );
   }
