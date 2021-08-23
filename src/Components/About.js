f2951d07b64eecb556009b4d92e81051e16f1bb2
Fernando-Paz <fernandopaz.ipn@gmail.com>
2021-08-23 02:00:34 -0500

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Fade from "react-reveal";
+
+class About extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const namesecond = this.props.data.namesecond;
+    const profilepic = "images/" + this.props.data.image;
+    const bio = this.props.data.bio;
+    const bio2 = this.props.data.bio2;
+    const bio3 = this.props.data.bio3;
+    const bio4 = this.props.data.bio4;
+    const street = this.props.data.address.street;
+    const city = this.props.data.address.city;
+    const state = this.props.data.address.state;
+    const zip = this.props.data.address.zip;
+    const phone = this.props.data.phone;
+    const email = this.props.data.email;
+    const email2 = this.props.data.email2;
+    const resumeDownload = this.props.data.resumedownload;
+
+    return (
+      <section id="about">
+        <Fade duration={1000}>
+          <div className="row">
+            {/* <div className="three columns">
+              <img
+                className="profile-pic"
+                src={profilepic}
+                alt="Nordic Giant Profile Pic"
+              />
+            </div> */}
+            <div className="nine columns main-col">
+              <h2>Can you tell me Paz</h2>
+              <p>{bio}<br></br>{bio2}<br></br>{bio3}<br></br>{bio4}</p>
+              <div className="row">
+                <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+                    <span>{name} {namesecond}</span>
+                    <br />
+                    <span>{phone}</span>
+                    <br />
+                    <span>{email}</span>
+                    <br />
+                    <span>{email2}</span>
+                  </p>
+                </div>
+                <div className="columns download">
+                  <p>
+                    <a href={resumeDownload} className="button">
+                      <i className="fa fa-download"></i> Resume
+                    </a>
+                  </p>
+                </div>
+              </div>
+              <div className="columns download">
+                <p></p>
+              </div>
+              <div className="columns download">
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/Components/Header.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import ParticlesBg from "particles-bg";
+import Fade from "react-reveal";
+
+class Header extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const resumeDownload = this.props.data.resumeDownload;
+    const name = this.props.data.name;
+    const namesecond = this.props.data.namesecond;
+    const description = this.props.data.description;
+    const description1 = this.props.data.description1;
+    const description2 = this.props.data.description2;
+
+    return (
+      <header id="home">
+        <ParticlesBg type="circle" bg={true} />
+
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#certi">
+                Certifications
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#eventos">
+                Events
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <Fade bottom>
+              <h1 className="responsive-headline">{name}</h1>
+              <h1 className="responsive-headline">{namesecond}</h1>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}</h3>
+              <h3>{description1}</h3>
+              <h3>{description2}</h3>
+            </Fade>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
